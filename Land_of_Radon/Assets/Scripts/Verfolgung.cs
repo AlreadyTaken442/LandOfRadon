9efae1350357bbee24e710946fc26a71e078f057
Maxim <113883267+AlreadyTaken442@users.noreply.github.com>
2023-03-08 10:50:32 +0100

    Enemy and the Script belonging to it
    Created the enemy  and a Script. I also made an enemy preset.

---
+++ Land_of_Radon/Assets/Scripts/Verfolgung.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Verfolgung : MonoBehaviour
+{
+    public Transform hackerObjekt;
+    public float verfolgungsRadius = 5f;
+    public float geschwindigkeit = 3f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float distanzZumHacker = Vector2.Distance(transform.position, hackerObjekt.position);
+        if (distanzZumHacker <= verfolgungsRadius)
+        {
+            Vector2 richtungZumHacker = (hackerObjekt.position - transform.position).normalized;
+            rb.velocity = richtungZumHacker * geschwindigkeit;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
