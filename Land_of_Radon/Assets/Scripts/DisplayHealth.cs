77c93e69a60300139a8e19b5ce0797f386da1d10
Maxim <113883267+AlreadyTaken442@users.noreply.github.com>
2023-03-10 09:57:57 +0100

    Edited the GUI
    the GUI is good, but not completed

--- Land_of_Radon/Assets/Scripts/DisplayHealth.cs
+++ Land_of_Radon/Assets/Scripts/DisplayHealth.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DisplayHealth : MonoBehaviour
 {
     public int health;
-    public Text healthText;
+    public TextMeshProUGUI healthText;
 
     private void Update()
     {
-        healthText.text = "HEALTH: " + health;
+        healthText.text = " : " + health;
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             health--;
+        }
+
+        if (health < 0)
+        {
+            health = 0;
         }
     }
 }
