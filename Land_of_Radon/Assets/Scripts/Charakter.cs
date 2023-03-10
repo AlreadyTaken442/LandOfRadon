04be3346eddb3f0f6c393d63bed4f132a46311cc
Maxim <113883267+AlreadyTaken442@users.noreply.github.com>
2023-03-10 10:28:04 +0100

    Few changes
    Nothing important

--- Land_of_Radon/Assets/Scripts/Charakter.cs
+++ Land_of_Radon/Assets/Scripts/Charakter.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Charakter : MonoBehaviour
+public class Charakter : MonoBehaviour // Definition der Charakter-Klasse
 {
-        public float speed;
-        [SerializeField] private InputActionReference movement;
-        private Rigidbody2D rigbod;
-        private Vector2 movementInput;
+    public float speed; // Geschwindigkeit des Charakters
+    [SerializeField] private InputActionReference movement; // Referenz auf eine InputAction, die die Bewegung des Charakters steuert
+    private Rigidbody2D rigbod; // Rigidbody2D-Komponente des Charakters
+    private Vector2 movementInput; // Eingabewerte für die Bewegung des Charakters
 
-        public Transform target;
+    public Transform target; // Ziel-Transform des Charakters (nicht verwendet)
 
-        private void Start()
-            {
-                rigbod = GetComponent<Rigidbody2D>();
-            }
-
-            // Update is called once per frame
-            void Update()
-            {
-                movementInput = movement.action.ReadValue<Vector2>();
-            }
-
-            private void FixedUpdate()
-            {
-                rigbod.velocity = movementInput * speed;
-            }
+    private void Start()
+    {
+        rigbod = GetComponent<Rigidbody2D>(); // Initialisierung des Rigidbody2D-Komponente
     }
+
+    void Update()
+    {
+        movementInput = movement.action.ReadValue<Vector2>(); // Eingabewerte für die Bewegung lesen
+    }
+
+    private void FixedUpdate()
+    {
+        rigbod.velocity = movementInput * speed; // Bewegung des Charakters berechnen und an den Rigidbody übertragen
+    }
+}
--- Land_of_Radon/Assets/Scripts/DisplayHealth.cs
+++ Land_of_Radon/Assets/Scripts/DisplayHealth.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using TMPro; //wichtig für Text-Objekt
 
 public class DisplayHealth : MonoBehaviour
 {
     public int health;
-    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI healthText; //TextMeshProUGUI ist für speziellen Text (TMro)
 
     private void Update()
     {
-        healthText.text = " : " + health;
+        healthText.text = " : " + health; //Output
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
