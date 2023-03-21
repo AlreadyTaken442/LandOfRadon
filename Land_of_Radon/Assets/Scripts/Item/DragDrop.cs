2c6cdfcdb1b47ae3936535f4c848ee1822b88f27
Mbloom69 <Mblum6948@gmail.com>
2023-03-21 12:27:41 +0100

    fertisch

--- Land_of_Radon/Assets/Scripts/Item/DragDrop.cs
+++ Land_of_Radon/Assets/Scripts/Item/DragDrop.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 
 //Interfaces
 public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    public Image image;
     public static GameObject itemBeingDragged;
     private GameObject canvas;
-    Vector3 startPosition;
-    Transform parentAfterDrag;
+    
+  [HideInInspector] public Transform parentAfterDrag;
 
     private void Awake()
     {
@@ -19,30 +21,32 @@ public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDrag
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
-       
         parentAfterDrag = transform.parent;
-
        transform.SetParent(canvas.transform);
         transform.SetAsLastSibling();
+        image.raycastTarget = false;
     }
 
-    public void OnDrag(PointerEventData eventData)
-    {
-       
-        Debug.Log(Input.mousePosition);
-       transform.position = Input.mousePosition;
-        Debug.Log("Test");
-       
-
-
-    }
 
     public void OnEndDrag(PointerEventData eventData)
     {
 
         transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
+
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+        transform.position = Input.mousePosition;
+        Debug.Log(Input.mousePosition);
+        Debug.Log("Test");
        
     }
+
+    
 }
  
 
