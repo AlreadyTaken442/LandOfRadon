486fd2113be12de94ce5c9aed2f046edb22011cd
Mbloom69 <Mblum6948@gmail.com>
2023-03-27 09:58:32 +0200

    ghg

--- Land_of_Radon/Assets/LootBag.cs
+++ Land_of_Radon/Assets/LootBag.cs
@@ -8,24 +8,34 @@ public class lootBag : MonoBehaviour
     public GameObject droppedItemPrefab;
     public List<Loot> lootlist = new List<Loot>();
 
-    Loot GetDroppedItem()
+    List<Loot> GetDroppedItems()
     {
         int randomNumer = Random.Range(1, 101); // 1-100  (erste und letze Zahl werden nicht gewerdet.)
         List<Loot> possibleItems = new List<Loot>();
         foreach (Loot item in lootList)
         {
-            if(randomNumber <= item.dropChance)
+            if (randomNumber <= item.dropChance)
             {
                 possibleItems.Add(item);
+                return possibleItems;
             }
         }
-    }
+            if (possibleItems.Count > 0)
+            {
+                Loot droppedItem = possibleItems[Random.Range(0, possibleItems.Count)];
+                return droppedItem;
+            }
+        Debug.Log("No Loot dropped");
+        return null;
 
 
-    // Start is called before the first frame update
-   /* void Start()
-    {
-       lootlist.Add(new Loot("Gem"), 80)); Das ist ein Beispiel wie es noch geht.
     }
-   */
-}
+        /*
+
+          // Start is called before the first frame update
+          void Start()
+          {
+             lootlist.Add(new Loot("Gem"), 80)); Das ist ein Beispiel wie es noch geht.
+          }
+         */
+    }
