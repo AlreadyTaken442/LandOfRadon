afde730a5a0e56c772fe3178adc0150667a9050b
Mbloom69 <Mblum6948@gmail.com>
2023-03-29 23:16:56 +0200

    Mage geupdated

--- Land_of_Radon/Assets/Scripts/Gegner/EnemyLongRange.cs
+++ Land_of_Radon/Assets/Scripts/Gegner/EnemyLongRange.cs
@@ -7,7 +7,8 @@ public class EnemyLongRange : MonoBehaviour
     public float speed;
     public float stoppingDistance;
     public float retreatDistance;
-
+    public float distancebetween;
+    private float distance;
     public GameObject projectille;
     private Transform player;
 
@@ -23,6 +24,7 @@ public class EnemyLongRange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        distance = Vector2.Distance(transform.position, player.position);
         if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
@@ -33,7 +35,7 @@ public class EnemyLongRange : MonoBehaviour
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
         }
 
-        if (timeBtwShots <= 0)
+        if (timeBtwShots <= 0 && distance < distancebetween)
         {
             Instantiate(projectille, transform.position, Quaternion.identity);
             timeBtwShots = startTimeBtwShots;
